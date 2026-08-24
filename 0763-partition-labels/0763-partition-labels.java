ef40bafa8308a021d196251984c577c66b814c7d
Hrithik Roshan <hrithikroshan1811@gmail.com>
2026-08-24 17:15:18 +0530

    Time: 5 ms (81.1%), Space: 43.3 MB (90.15%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> result = new ArrayList<>();
+
+        int[] last = new int[26];
+
+        // Store last occurrence of every character
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+
+            if (i == end) {
+                result.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
+    }
+}
